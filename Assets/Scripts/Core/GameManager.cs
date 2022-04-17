1bd6d79672e89e1e70987e02f04038fc08726186
Minzkraut <git@minzkraut.com>
2022-04-17 21:57:09 +0200

    Add player controls and trail

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -47,6 +47,6 @@ public class GameManager : MonoBehaviour
 
     public void IncrementScore(int val)
     {
-
+        
     }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,24 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public int health;
+    public float health;
     public float speed;
     public float trailLength;
-    public ParticleSystem trail;
+    public GameObject trail;
+    public float rotationSpeed;
+    public float direction;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public bool isLit;
+
+    public Camera camera;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 inputVector = value.Get<Vector2>();
+        direction = inputVector.x;        
         
     }
+
 
     // Update is called once per frame
     void Update()
     {
+
+        //if isLit decrease health
+        if (isLit)
+        {
+            health -= 100.0f * Time.deltaTime;
+            Debug.Log("Health: " + health);
+        }
+
+        //move player forward
+        transform.position += transform.forward * speed * Time.deltaTime;
+        //rotate based on direction
+        transform.Rotate(0, direction * rotationSpeed * 2 * Time.deltaTime, 0);
+
+        float horzExtent = camera.orthographicSize * Screen.width / Screen.height;
+        float vertExtent = camera.orthographicSize;
+
+        if (transform.position.z > vertExtent)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -vertExtent);
+        }
+        else if (transform.position.z < -vertExtent)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, vertExtent);
+        }
         
+        if (transform.position.x > horzExtent)
+        {
+            transform.position = new Vector3(-horzExtent, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -horzExtent)
+        {
+            transform.position = new Vector3(horzExtent, transform.position.y, transform.position.z);
+        }
     }
 
     public void DecreaseTrail(float val)
@@ -31,11 +80,12 @@ public class Player : MonoBehaviour
         //Increate trail length
     }
 
+    
     private void OnParticleCollision(GameObject other)
     {
         if (other == trail)
         {
-            //Hit by trail
+            //Debug.Log(other.name);
         }
     }
 }
